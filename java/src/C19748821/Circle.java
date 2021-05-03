37d2308d04e7de4a2f8da4a0cb943212cb76e2d1
Alibaba3222 <aliananza1990@gmail.com>
2021-05-03 16:29:51 +0100

    commit 3

--- java/src/C19748821/Circle.java
+++ java/src/C19748821/Circle.java
@@ -20,4 +20,34 @@ public class Circle{
 
     int fillCircle = 0;
 
- 
+    PApplet gfx;
+    public Circle(PApplet gfx){
+        this.gfx = gfx;
+
+        width = gfx.width;
+        height = gfx.height;
+    }
+
+    void update(Circle[] balls, float soundValue){
+        float hue = map(soundValue*500, -400, 400, 0, 255);
+
+        angle += speed;
+
+        //determining the x and y pos of circle based on the sound frequency value
+        xPos = (float) Math.cos(gfx.radians(angle)) * roundRadius * 5f;
+        yPos = (float) Math.sin(gfx.radians(angle)) * roundRadius * 3f;
+
+        for (Circle ball : balls) {
+            if (ball != this) {
+
+                //this code draws lines from each circle to every other circle, creating a mesh
+                gfx.stroke(hue, 100, 100);
+                gfx.line(xPos, yPos, ball.xPos, ball.yPos);
+            }
+        }
+
+        gfx.noStroke();
+        gfx.fill(hue, 255, 255);
+        gfx.circle(xPos, yPos, radius);
+    }
+}
