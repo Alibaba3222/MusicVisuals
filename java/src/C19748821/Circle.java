5f1a533bdd168b8c78677c4f5bf4587a45117c75
Alibaba3222 <aliananza1990@gmail.com>
2021-05-03 16:22:17 +0100

    circle 1

---
+++ java/src/C19748821/Circle.java
@@ -0,0 +1,23 @@
+package C19748821;
+
+import processing.core.PApplet;
+import static processing.core.PApplet.map;
+
+public class Circle{
+    float speed;
+    float angle;
+    float xPos;
+    float yPos;
+
+    float width;
+    float height;
+
+    float xVel;
+    float yVel;
+
+    float roundRadius;
+    float radius;
+
+    int fillCircle = 0;
+
+ 
